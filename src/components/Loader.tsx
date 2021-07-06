db2885914a2f33192ddf42954a250c3676e1dd44
puneetsingh99 <99singhpuneet99@gmail.com>
2021-07-06 23:33:21 +0530

    fix: navbar UI

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <article className="fixed w-6/12 h-2/5 flex justify-center items-center">
+    <article className="fixed w-6/12 h-2/5 r-0 l-0 flex justify-center items-center">
       Loading...
     </article>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { NavDesktop } from "./NavDesktop";
 
 export const Navbar: React.FC = () => {
   return (
-    <header className="sticky top-0 w-screen sm:p-2  sm:px-4 sm:py-0 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800 shadow-sm">
+    <header className="sticky z-10 top-0 w-full sm:p-2  sm:px-4 sm:py-0 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800 shadow-sm">
       <div className="flex flex-col items-center justify-between sm:flex-row sm:w-4/6 m-auto">
         <div className="text-xl font-logo flex items-center font-semibold cursor-pointer">
           <h1>Brain</h1>
