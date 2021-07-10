1887b81cb1cf77fac32f683fb1b8a40954669882
puneetsingh99 <99singhpuneet99@gmail.com>
2021-07-10 09:05:17 +0530

    refactor: empty leaderboard scenario

--- src/pages/quiz-detail/leaderboard/LeaderBoard.tsx
+++ src/pages/quiz-detail/leaderboard/LeaderBoard.tsx
@@ -6,6 +6,9 @@ export const LeaderBoard = ({ topScorers }: { topScorers: TopScorer[] }) => {
   console.log(topScorers);
   return (
     <article className="w-325 h-500 sm:w-375 sm:h-450 text-lg text-gray-900 dark:text-gray-100 bg-white dark:bg-gray-800 p-4 rounded-xl shadow-md">
+      <div className="w-full flex justify-center px-4 pb-4 sm:pb-0 font-semibold">
+        <h1 className="uppercase tracking-wider text-xl">Leaderboard</h1>
+      </div>
       <ul>
         <li className="flex justify-between items-center mb-2 font-semibold ">
           <div>
@@ -19,12 +22,8 @@ export const LeaderBoard = ({ topScorers }: { topScorers: TopScorer[] }) => {
           </div>
         </li>
         {topScorers.length === 0 && (
-          <div className="w-full h-400 sm:h-350 flex flex-col justify-center items-center text-xl">
-            <p className="text-center mb-4 font-bold">Claim your Throne!</p>
-            <img src={throne} alt="throne" className="w-2/4" />
-            <p className="text-center mt-4 font-bold">
-              Be the first one to take this quiz
-            </p>
+          <div className="w-full h-400 sm:h-350 flex justify-center items-start">
+            <img src={throne} alt="throne" className="w-2/4 mt-8" />
           </div>
         )}
         {topScorers.length > 0 &&
