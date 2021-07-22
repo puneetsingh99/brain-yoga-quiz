bf9f97bac53917f303f97c06349015b846b91e1f
puneetsingh99 <puneetsingh99@github.com>
2021-07-22 15:55:51 +0530

    feat: modal design

--- src/pages/private/take-quiz/components/Modal.tsx
+++ src/pages/private/take-quiz/components/Modal.tsx
@@ -28,12 +28,32 @@ export const Modal = ({
   };
 
   return (
-    <section className={`border-4 border-red-500`}>
-      <h1>{showSubmitQuizModal ? `Submit Quiz modal` : `Quit Quiz Modal`}</h1>
-      <button onClick={handleShowHide}>Cancel</button>
-      <button onClick={handleSubmitQuit}>
-        {showSubmitQuizModal ? `Submit` : `Quit`}
-      </button>
+    <section
+      className={`flex items-center justify-center fixed inset-0 z-40 bg-semi-transparent`}
+    >
+      <div
+        className={`w-11/12 sm:w-200 py-4 px-2 rounded-2xl bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-100`}
+      >
+        <h1 className={`text-2xl mb-16 text-center`}>Are you sure?</h1>
+        <div className={`flex justify-center`}>
+          <button
+            className={`px-3 py-2 mr-6 rounded-lg dark:bg-gray-700 bg-gray-100 dark:hover:bg-gray-600 bg-gray-200 font-semibold text-white transition duration-200 ease-in-out focus:ring-4 ring-blue-500 ring-opacity-60`}
+            onClick={handleShowHide}
+          >
+            Cancel
+          </button>
+          <button
+            className={`px-3 py-2 rounded-lg ${
+              showSubmitQuizModal
+                ? `gradient-bg`
+                : `bg-blue-500 hover:bg-blue-600`
+            }  font-semibold text-white transition duration-100 ease-in-out cursor-pointer focus:ring-4 ring-blue-500 ring-opacity-60`}
+            onClick={handleSubmitQuit}
+          >
+            {showSubmitQuizModal ? `Submit` : `Quit`}
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
