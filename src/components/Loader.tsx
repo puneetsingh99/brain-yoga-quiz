262a0c41f449b532e46b4e9c283d79bb2d8b9543
puneetsingh99 <puneetsingh99@github.com>
2021-07-22 16:30:52 +0530

    fix: loader height

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,6 @@
 export const Loader = () => {
   return (
-    <article className="m-auto w-6/12 h-400 flex justify-center items-center">
+    <article className="m-auto w-6/12 max-h-400 flex justify-center items-center">
       <div className="spinner"></div>
     </article>
   );
